2847fa94b42395f697c1597cf0206535f25abedf
BRAJESH  VERMA <vermabrajesh12@gmail.com>
2018-07-05 10:07:45 +0530

    init commit

---
+++ server/server.js
@@ -0,0 +1,12 @@
+const path=require('path');
+const publicPath=path.join(__dirname,'../public');
+const express=require('express');
+var app=express();
+const port=process.env.PORT||3000;
+app.use(express.static(publicPath));
+app.listen(port,()=>{
+  console.log(`server is up on port${port}`);
+});
+
+// console.log(__dirname+'/../public');
+// console.log(publicPath);
